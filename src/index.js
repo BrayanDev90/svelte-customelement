c4231fe8d284dd91823cb34906dac6caab55fcd5
brayandev90 <bpa787448@gmail.com>
2023-05-07 22:23:07 -0700

    change module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Component from './Component.wc.svelte';
 import OtherComponent from './OtherComponent.svelte';
-import NestedComponent from './NestedComponent.svelte';
+import NestComponent from './NestedComponent.svelte';
 
 export {
   Component,
   OtherComponent,
-  NestedComponent,
+  NestComponent,
 };
